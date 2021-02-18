440011856df8a2fd524c49ba8a92aa62f153e7fc
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-02-19 02:54:08 +0300

    fix

--- my-app/src/api/News/provider.js
+++ my-app/src/api/News/provider.js
@@ -1,8 +1,8 @@
-// import { converter } from "./constasts/news";
-// import { REQUEST_URL } from '../converter'
+import { converter } from "./constasts/news";
+import { REQUEST_URL } from '../converter'
 
-// const F = () => (
-//     fetch('01cfd4ad10a7443f9e6dc750705de9c1')
-//     .then((response) => (response.json()))
-//     .then((data) => converter(data) )
-// )
+const F = () => (
+    fetch('01cfd4ad10a7443f9e6dc750705de9c1')
+    .then((response) => (response.json()))
+    .then((data) => converter(data) )
+)
