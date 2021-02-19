406036353348ad093c9caf5d9e7a70f4b43ccbe3
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-02-19 03:31:38 +0300

    fix

--- my-app/src/api/News/provider.js
+++ my-app/src/api/News/provider.js
@@ -1,8 +1,10 @@
-import { converter } from "./constasts/news";
-import { REQUEST_URL } from '../converter'
 
-const F = () => (
-    fetch('01cfd4ad10a7443f9e6dc750705de9c1')
+
+import { REQUEST_URL } from '../constants/news';
+import { converter } from './converter';
+
+export const getNews = () => (
+  fetch('01cfd4ad10a7443f9e6dc750705de9c1')
     .then((response) => (response.json()))
-    .then((data) => converter(data) )
-)
+    .then((data) => converter(data))
+);
