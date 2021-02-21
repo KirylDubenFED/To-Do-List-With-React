cc087945d2960ca9e58bb431fa1241ebc104f705
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-02-22 00:38:33 +0300

    add Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,11 @@
-const Button = () => {
+import React from 'react';
+
+const Button = ({ children }) => {
   return (
-        
-    )
-}
+    <button>
+      {children}
+    </button>
+  )
+};
+
+export default Button;
