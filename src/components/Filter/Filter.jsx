3c277b03688af3c1c610b83809feb219b9d37861
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-02-24 01:19:29 +0300

    fix Filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import getFilters from '../api/filterApi/provider';
 
 const Filter = () => {
+  const [filters, setFilters] = useState([]);
   useEffect(() => {
-    getFilters().then((data) => console.log(data));
+    getFilters().then((data) => setFilters(data));
   });
+
+  console.log(filters);
 
   return (
     <button type="button">Текущие задачи</button>
