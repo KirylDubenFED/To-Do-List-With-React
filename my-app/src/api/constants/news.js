af93b58caba262141f5cb8b53931be4f2875a310
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-03-05 19:33:19 +0300

    fix api

--- my-app/src/api/constants/news.js
+++ my-app/src/api/constants/news.js
@@ -1,3 +1,3 @@
 const API_KEY = '01cfd4ad10a7443f9e6dc750705de9c1';
 export const REQUEST_URL =
-  `http://newsapi.org/v2/everything?q=tesla&from=2021-01-19&sortBy=publishedAt&apiKey=${API_KEY}`;
+  `http://newsapi.org/v2/everything?q=tesla&from=2021-02-05&sortBy=publishedAt&apiKey=${API_KEY}`;
