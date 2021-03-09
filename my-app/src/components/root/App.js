f234b7a1e2013a8cfa65493187d50b6bee2df668
KirylDubenFED <69189757+KirylDubenFED@users.noreply.github.com>
2021-03-09 18:59:34 +0300

    fix app

--- my-app/src/components/root/App.js
+++ my-app/src/components/root/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Search from '../Search/Search'
 import { getNews } from '../../api/News/provider';
 // import { NavLink } from 'react-router-dom';
 // import { Switch, Route } from 'react-router-dom';
@@ -11,21 +10,7 @@ function App() {
 
   return (
     <div className='App'>
-      
-      <Search />
-      {/* <nav>
-        <ul>
-          <li><NavLink exact to="/" activeClassName="selected">Home</NavLink></li>
-          <li><NavLink exact to="/contacts" activeClassName="selected">contacts</NavLink></li>
-          <li><NavLink exact to="/about-us" activeClassName="selected">about us</NavLink></li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route exact path="/"><HomePage /></Route>
-        <Route path="/contacts"><ContactsPage /></Route>
-        <Route path="/about-us"><AboutPage /></Route>
-        <Route path="*"><PageNotFound /></Route>
-      </Switch> */}
+
     </div>
   );
 }
